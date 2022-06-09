a2ee7db410108049b56e9cba06b63384456b05dd
ladetunji <ladetunji.osibanjo@gmail.com>
2022-06-09 16:55:23 -0700

    Task 10 - commit 1

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle
+ *
+ * @size: size of triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+{
+	for (j = size; j > i; j--)
+	{
+		_putchar(' ');
+		}
+		for (k = 1; k <= j; k++)
+		{
+		har('#');
+		}
+		_putchar('\n');
+		}
+	}
+}
