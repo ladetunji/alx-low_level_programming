de309c5178e2c5a7e19dab07a8f36eebb7de07bd
ladetunji <ladetunji.osibanjo@gmail.com>
2022-06-09 05:52:14 -0700

    Task 0 - commit 2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 
 int main(void)
+
 {
 	int i;
 
