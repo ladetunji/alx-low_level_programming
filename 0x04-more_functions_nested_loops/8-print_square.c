9ee0355c4cc34b67242ab74e065f7c79b0c878c3
ladetunji <ladetunji.osibanjo@gmail.com>
2022-06-09 16:39:01 -0700

    Task 9 complete

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,27 @@
-#include "main.h"
+include "main.h"
 
 /**
  * print_square - func that prints 10 times the numbers, from 0 to 14
  *
  * @size:  integer return type
  */
+
 void print_square(int size)
 {
 	int i = 0, j;
 
-	if (size <= 0)
-	{
+
+		if (size <= 0)
+		{
 		_putchar('\n');
 		}
 		else
-		{
+					{
 		while (i < size)
+		{
 		j = 0;
 
 		while (j < size)
-
 		{
 		_putchar('#');
 
