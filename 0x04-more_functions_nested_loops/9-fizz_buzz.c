e43c29b7dd34c3d41c72caaf3ee4323a43a9f5c4
ladetunji <ladetunji.osibanjo@gmail.com>
2022-06-10 07:43:06 -0700

    Task 9 - rectified brackets

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,7 @@
 
 /**
  * main - prints the Fizz-Buzz test
+ * 
  * Return: Always return 0
  */
 int main(void)
@@ -12,23 +13,23 @@ int main(void)
 
 	while (i < 101)
 	{
-	if (i % 3 == 0 && i % 5 == 0)
-	{
-	printf(" FizzBuzz");
-	}
-	else if (i % 3 == 0)
-	printf(" Fizz");
-	}
-	else if (i % 5 == 0)
-	{
-	printf(" Buzz");
-	}
-	else
-	{
-	printf(" %d", i);
-	}
-	i++;
-
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf(" FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+		printf(" Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+		printf(" Buzz");
+		}
+		else
+		{
+		printf(" %d", i);
+		}
+		i++;
 	}
 	printf("\n");
 	return (0);
