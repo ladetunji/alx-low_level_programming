8982744417768b61fefa7ba088e579ce351a9139
ladetunji <ladetunji.osibanjo@gmail.com>
2022-06-13 15:54:06 -0700

    Task 6 - commit 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,12 +13,14 @@ void puts2(char *str)
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
-	{
+		{
 		_putchar('\n');
 		break;
-	}
-	if (count % 2 == 0)
+		}
+		if (count % 2 == 0)
+		{
 		_putchar(str[count]);
 		count++;
+		}
 	}
 }
